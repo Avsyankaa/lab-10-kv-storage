6e97466ebdf1f4a4481df206f2e4b6817df7b353
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2019-01-05 00:14:34 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,7 +8,7 @@ TEST(Kv_storage, Test1) {
     kv_storage kv;
     kv.set_log_level("warning");
     kv.set_thread_count(4);
-    kv.set_path_write("/home/vagrant/Projects/lab-10-kv-storage/hash");
+    kv.set_path_write("/home/travis/build/Avsyankaa/lab-10-kv-storage/hash");
     kv.make_process();
     SUCCEED();
 }
